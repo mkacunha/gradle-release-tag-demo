d9877302751763700ba57d8e166f5c6f80e9b8ef
Maiko Cunha <maicunha@uolinc.com>
2022-12-01 15:11:59 -0300

    added release plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,10 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.7.10"
     application
+    id("net.researchgate.release") version "2.8.1"
 }
+
+apply(from = "./gradle/release.gradle")
 
 group = "com.mkacunha"
 version = "1.0-SNAPSHOT"
